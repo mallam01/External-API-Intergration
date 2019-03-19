c8a2cf83c199e8ec88b89bf507e4534cd2d20c1e
mallam01 <m.roushdy.allam@gmail.com>
2019-03-18 23:20:39 -0400

    update junit test cases

--- src/test/java/com/webservice/ExternalAPIIntergration/resource/ExternalResourceUnitTest.java
+++ src/test/java/com/webservice/ExternalAPIIntergration/resource/ExternalResourceUnitTest.java
@@ -2,29 +2,21 @@ package com.webservice.ExternalAPIIntergration.resource;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import javax.inject.Inject;
-import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.test.JerseyTest;
-import org.glassfish.jersey.test.TestProperties;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.webservice.ExternalAPIIntergration.service.ExternalService;
 
-
-public class ExternalResourceUnitTest{
-	
-	/**
+public class ExternalResourceUnitTest {
 
 	@InjectMocks
 	ExternalResource externalResource;
@@ -45,12 +37,11 @@ public class ExternalResourceUnitTest{
 		assertEquals(mockResponse, actualResponse);
 
 	}
-	
+
 	@Test
 	public void testCallServiceOnce() {
-		externalService.retireveAllData(); 
-		verify(externalService,times(1) ).retireveAllData();
+		externalService.retireveAllData();
+		verify(externalService, times(1)).retireveAllData();
 	}
-	**/
 
 }
